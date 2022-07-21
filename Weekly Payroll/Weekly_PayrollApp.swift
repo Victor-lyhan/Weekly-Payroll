8648d351092e11c315ead19af29a4f82b34ae3b4
Victor Han <liyanghan1112@gmail.com>
2022-07-21 14:58:49 -0500

    Initial Commit

---
+++ Weekly Payroll/Weekly_PayrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekly_PayrollApp.swift
+//  Weekly Payroll
+//
+//  Created by Victor Han on 2022/7/19.
+//
+
+import SwiftUI
+
+@main
+struct Weekly_PayrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
